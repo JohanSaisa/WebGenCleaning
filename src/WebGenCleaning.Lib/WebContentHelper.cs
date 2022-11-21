65ef56d2a9182f95e764c7c9e7df3e277df0ab5b
Rasmus Broborg <43502647+RasmusBroborg@users.noreply.github.com>
2022-11-21 13:37:49 +0100

    Refactor FormatSentences method.

--- src/WebGenCleaning.Lib/WebContentHelper.cs
+++ src/WebGenCleaning.Lib/WebContentHelper.cs
@@ -10,36 +10,54 @@ public static class WebContentHelper
     {HtmlTag.Paragraph, "p"},
     {HtmlTag.Heading, "h1"},
   };
-  
+
   private static readonly string[] HtmlStart = new string[] { "<!DOCTYPE html>", "<html>", "<body>", "<main>", };
   private static readonly string[] HtmlEnd = new string[] { "</main>", "</body>", "</html>", };
-  
+
   public static string[] FormatSentences(string[] textToBeFormatted)
   {
     var formattedText = new List<string>();
     foreach (var text in textToBeFormatted)
     {
-      var regEx = new Regex(@"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s");
-      var sentences = regEx.Split(text);
+      string[] sentences = ExtractSentences(text);
 
-      var formattedSentence = "";
-
-      var index = 0;
+      var formattedSentence = String.Empty;
       foreach (var sentence in sentences)
       {
-        var t = sentence.Trim().ToLower();
-        
-        if (index > 0)
-          formattedSentence += " ";
-        
-        formattedSentence += char.ToUpper(t[0]) + t[1..];
-        index++;
+        var trimmedSentence = sentence.Trim().ToLower();
+
+        if (NotFirstSentence(sentence, sentences))
+          AddBlankspaceBeforeSentence(ref formattedSentence);
+
+        formattedSentence += AddUppercaseToFirstLetter(trimmedSentence);
       }
 
       formattedText.Add(formattedSentence);
     }
 
     return formattedText.ToArray();
+  }
+
+  private static bool NotFirstSentence(string sentence, string[] sentences)
+  {
+    return !(sentence == sentences[0]);
+  }
+
+  private static void AddBlankspaceBeforeSentence(ref string sentence)
+  {
+    sentence += " ";
+  }
+
+  private static string AddUppercaseToFirstLetter(string trimmedSentence)
+  {
+    return char.ToUpper(trimmedSentence[0]) + trimmedSentence[1..];
+  }
+
+  private static string[] ExtractSentences(string text)
+  {
+    var sentenceRegex = new Regex(@"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s");
+    var sentences = sentenceRegex.Split(text);
+    return sentences;
   }
 
   public static string GenerateHtml(HtmlTag htmlTag, string input)
