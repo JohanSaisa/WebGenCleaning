8d738dae3335fba86f1bc9c57d5c632171fae588
Johan Säisä <89136019+JohanSaisa@users.noreply.github.com>
2022-11-21 11:05:25 +0100

    Add test for paragraph builder. Fix bug in formatter test.

--- tests/WebGenCleaning.Lib.Test/UnitTest1.cs
+++ tests/WebGenCleaning.Lib.Test/UnitTest1.cs
@@ -1,10 +1,47 @@
+using FluentAssertions;
+
 namespace WebGenCleaning.Lib.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void FormatSentencesTest()
     {
+      // Arrange
+      string[] coursesFromApi = {"   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   "};
+      string[] expectedCourses = {"C#", "Databaser", "Webbutveckling", "Clean code"};
+      
+      // Act
+      string[] formattedCourses = WebContentHelper.FormatSentences(coursesFromApi);
 
+
+      // Assert
+      formattedCourses.Should().Equal(expectedCourses);
+    }
+
+    [Theory]
+    [InlineData("C#", "<p>C#</p>")]
+    [InlineData("Databaser", "<p>Databaser</p>")]
+    [InlineData("Webbutveckling", "<p>Webbutveckling</p>")]
+    [InlineData("Clean code", "<p>Clean code</p>")]
+    public void BuildParagraphTest(string input, string expectedResult)
+    {
+      // Arrange
+      
+      // Act
+      string result = WebContentHelper.GenerateParagraph(input);
+
+      // Assert
+      result.Should().Be(expectedResult);
     }
 }
+
+
+// Template
+// Arrange
+      
+      
+// Act
+      
+      
+// Assert
